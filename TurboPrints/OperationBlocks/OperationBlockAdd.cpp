8c4dd8ef065b4cf7adff8d64eff2ce015e4f8952
VladimirR <rvitaminka20@gmail.com>
2023-02-19 15:29:41 +0300

    fix operation add asm

--- TurboPrints/OperationBlocks/OperationBlockAdd.cpp
+++ TurboPrints/OperationBlocks/OperationBlockAdd.cpp
@@ -2,5 +2,5 @@
 
 void OperationBlockAdd::CompileBlock(std::ofstream& out)
 {
-	out << "\tADD " << InputsOperands->at(0)->Text << ", " << InputsOperands->at(0)->Text << std::endl;
+	out << "\tADD " << InputsOperands->at(0)->Text << ", " << InputsOperands->at(1)->Text << std::endl;
 }
